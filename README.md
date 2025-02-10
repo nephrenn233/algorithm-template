7675344455dd2af7b962462d50d43d40efc53e45
nephrenn233 <nephrenn233@outlook.com>
2025-02-11 00:08:58 +0800

    update segment_tree_1

--- README.md
+++ README.md
@@ -14,7 +14,7 @@
 + 树状数组 (fenwick_tree.cpp)，支持单点修改、前缀和查询
 + 并查集 (disjoint_set_union.cpp)，支持查询集合、查询集合大小、合并
 + 线段树
-    + 区间加，单点改 (segment_tree_1.cpp)
+    + 单点修改，区间查询 (segment_tree_1.cpp, with Info)
 + 稀疏表 (sparse_table.cpp, with Info)
 
 ### 数学 math
--- data_structure/segment_tree_1.cpp
+++ data_structure/segment_tree_1.cpp
@@ -1,8 +1,8 @@
 struct segment_tree {
     int n;
-    std::vector<int> tr, lazy;
-    segment_tree(int _n) : n(_n), tr(n * 4), lazy(n * 4) {}
-    segment_tree(const std::vector<int> &a) : n(a.size()), tr(n * 4), lazy(n * 4) {
+    std::vector<Info> tr;
+    segment_tree(int _n) : n(_n), tr(_n * 4) {}
+    segment_tree(const std::vector<Info> &a) : n(a.size()), tr(n * 4) {
         auto build = [&](auto &&self, int u, int l, int r) -> void {
             if (r - l == 1) {
                 tr[u] = a[l];
@@ -18,63 +18,32 @@ struct segment_tree {
     void pull(int u) {
         tr[u] = tr[u * 2] + tr[u * 2 + 1];
     }
-    void push(int u, int l, int r) {
-        if (lazy[u]) {
-            tr[u] += lazy[u] * (r - l);
-            if (r - l > 1) {
-                lazy[u * 2] += lazy[u];
-                lazy[u * 2 + 1] += lazy[u];
-            }
-            lazy[u] = 0;
-        }
-    }
-    void range_add(int u, int l, int r, int ql, int qr, int qv) {
-        if (qr <= l || r <= ql) {
-            return;
-        }
-        if (ql <= l && r <= qr) {
-            tr[u] += qv * (r - l);
-            lazy[u] += qv;
-            return;
-        }
-        push(u, l, r);
-        int mid = (l + r) / 2;
-        range_add(u * 2, l, mid, ql, qr, qv); 
-        range_add(u * 2 + 1, mid, r, ql, qr, qv);
-        pull(u);
-    }
-    void range_add(int ql, int qr, int qv) {
-        range_add(1, 0, n, ql, qr, qv);
-    }
-    void modify(int u, int l, int r, int qp, int qv) {
+    void modify(int u, int l, int r, int qp, const Info &qv) {
         if (r - l == 1) {
             tr[u] = qv;
             return;
         }
-        push(u, l, r);
         int mid = (l + r) / 2;
-        if (qp < mid) {
-            modify(u * 2, l, mid, qp, qv);
-        } else {
-            modify(u * 2 + 1, mid, r, qp, qv);
-        }
+        if (qp < mid) modify(u * 2, l, mid, qp, qv);
+        else modify(u * 2 + 1, mid, r, qp, qv);
         pull(u);
     }
-    void modify(int qp, int qv) {
+    void modify(int qp, const Info &qv) {
         modify(1, 0, n, qp, qv);
     }
-    int range_query(int u, int l, int r, int ql, int qr) {
+    Info range_query(int u, int l, int r, int ql, int qr) {
         if (qr <= l || r <= ql) {
-            return 0;
+            return {};
         }
         if (ql <= l && r <= qr) {
             return tr[u];
         }
-        push(u, l, r);
         int mid = (l + r) / 2;
         return range_query(u * 2, l, mid, ql, qr) + range_query(u * 2 + 1, mid, r, ql, qr);
     }
-    int range_query(int ql, int qr) {
+    Info range_query(int ql, int qr) {
+        // assert(ql < qr);
+        if (ql >= qr) return {};
         return range_query(1, 0, n, ql, qr);
     }
 };
