8c65becf2db39411fbcd13be0f47ebabf39470ba
nephrenn233 <nephrenn233@outlook.com>
2025-04-07 20:17:39 +0800

    update sparse_table

--- cpp/data_structure/sparse_table.cpp
+++ cpp/data_structure/sparse_table.cpp
@@ -2,11 +2,10 @@ struct sparse_table {
     int n;
     std::vector<std::vector<Info>> a;
 
-    template <class T>
-    sparse_table(const std::vector<T> &v) : n(v.size()) {
+    sparse_table(const std::vector<Info> &v) : n(v.size()) {
         a.assign(std::__lg(n) + 1, std::vector<Info>(n));
         for (int i = 0; i < n; ++i) {
-            a[0][i] = Info(v[i]);
+            a[0][i] = v[i];
         }
         for (int i = 1; i <= std::__lg(n); ++i) {
             for (int j = 0; j + (1 << i) <= n; ++j) {
